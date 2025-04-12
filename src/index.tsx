45a5cb7ceddb05fc337b7f66bcffb64b5a9e7f6f
Benjamín Giampaoli <blflores@uc.cl>
2025-04-11 20:36:10 -0400

    feat(TypeScript): Pase el proyecto a TypeScript

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
-import Routing from "./Routing.jsx";
+import Routing from "./Routing.tsx";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<Routing />
 	</StrictMode>
