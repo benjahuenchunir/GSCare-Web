cf08136a6fef8c6c1330fdd9bd977eb783817ce1
Felipe Espinoza <felipe.espinozas@uc.cl>
2025-04-29 10:04:10 -0400

    Revert "Merge branch 'Servicios' into develop"
    This reverts commit 2267537fed91d5ee1dab8c7be0e90d26c1e14183, reversing
    changes made to a24ad0d372c837f47d31a4d835de6fb0fe9f5a03.

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,20 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./Layout"; // nuevo
 import App from "./App";
-import ServicePage from "./pages/Servicios/servicePage";
-import ServicesListPage from "./pages/Servicios/ServicesListPage"; // Nuevo
 
 export default function Routing() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<App />} />
-          <Route path="/servicios" element={<ServicesListPage />} />
-          <Route path="/servicios/:id" element={<ServicePage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<App />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
 }
-
